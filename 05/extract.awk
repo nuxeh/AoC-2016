499305941953f65e74fc530ca67ec8c239514d8b
Edward Cragg <drq.11235@gmail.com>
2016-12-05 23:31:31 +0000

    05-02: finishing touches
        $ ./find-password.py | ./extract.awk
        0000027b9705c7e6fa3d4816c490bbfd |__7_____|
        00000468c8625d85571d250737c47b5a |__7_6___|
        0000013e3293b49e4c78a5b43b21023b |_37_6___|
        0000040bbe4509b48041007dec6123bd |_37_6___|
        00000b11810477f9e49840991fb2151e |_37_6___|
        00000cc461c8945671046cf632be4473 |_37_6___|
        000007c1da6865df78b2c0addf28913d |_37_6__c|
        00000700ce8beb0a8ffc83fa9986d577 |_37_6__c|
        00000d76f344ea0a8643bb2bcf0323c4 |_37_6__c|
        0000050301b17d598b52e2a343b80c95 |_37_60_c|
        000006fe7545b487de2d003f3d4e1114 |_37_60fc|
        000002d5272322770ff5e60b01455753 |_37_60fc|
        000004f449e36332d8693c3a84f8bf70 |_37_60fc|
        000002f6b67f02f6c7565a041503ea65 |_37_60fc|
        0000022f1d102210cb4a6c9dd8cb6715 |_37_60fc|
        000006fbc50b5e7d1e4c1c390bfc9370 |_37_60fc|
        000003e432ea631581aefcce573d56dd |_37e60fc|
        00000bb78c4ed10825e030cd1c239ad1 |_37e60fc|
        0000098a3982c7135ad8ad1999ba98a5 |_37e60fc|
        0000053c41ec1a808dac438da19486ac |_37e60fc|
        000001922d8af3ae96feff1a35e2414f |_37e60fc|
        00000379365fc17224a1e1e3b672ab02 |_37e60fc|
        00000eb02c7276f40e60a8e8e2185828 |_37e60fc|
        000006a9336d4ae05f2dbff912e00604 |_37e60fc|
        00000740e2fd901d7f98f007b48771cd |_37e60fc|
        00000155747b21cea30ef0222d0e7f3c |_37e60fc|
        000002f49ca6360dea1cd854d9032331 |_37e60fc|
        00000048d155e2c930602533209b0154 |437e60fc|

--- 05/extract.awk
+++ 05/extract.awk
@@ -1,4 +1,7 @@
 #!/usr/bin/awk -f
+# Build a password of 8 characters from fields where the 6th character (if
+# 0-7) is an address, and the 7th character is a value to insert. Do not
+# overwrite if the slot in the password has already been assigned.
 
 BEGIN {FS=""; count=0; ORS=""
 	split("________", a, "")
@@ -6,16 +9,15 @@ BEGIN {FS=""; count=0; ORS=""
 
 {
 	gsub(/\s.*$/, "", $0)
-	print $0 "\n"
+	print $0 " "
 	if ($6 >= 0 && $6 <= 7) {
 		if (a[$6+1] == "_") {
 			++count;
 			a[$6+1] = $7
 		}
-		echo()
-		if (count == 8)
-			exit
 	}
+	echo()
+	if (count == 8)	exit
 }
 
 function echo(){
--- 05/find-password.py
+++ 05/find-password.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python2
+# Generate a string with fixed input data, concatenated with an increasing
+# integer, and checksum the resulting string. Print checksums starting with 5
+# zeroes (valid data) and take the 6th character in the string as being a
+# character in the password, concatenated in order of discovery into a final
+# password string.
+#
+# Stop after 8 characters if printing to a terminal, or continuously output
+# valid checksums if output is redirected.
 
 import md5
 import sys
@@ -9,7 +17,6 @@ num = 0
 count = 0
 password = ""
 
-
 while True:
     m = md5.new()
     m.update(input + str(num))
@@ -17,8 +24,10 @@ while True:
 
     if hex_string[0:5] == '00000':
         password += hex_string[5]
+
 	print hex_string + " " + password
 	sys.stdout.flush()
+
 	if len(password) == 8 and sys.stdout.isatty():
             print "Password is: " + password
             exit()
