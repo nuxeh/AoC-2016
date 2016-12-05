8b64f0d2d1b8aa56edbde364eab3ada6f134d622
Edward Cragg <edward.cragg@codethink.co.uk>
2016-12-05 16:55:22 +0000

    05-01: print codes

--- 05/find-password.sh
+++ 05/find-password.sh
@@ -5,4 +5,4 @@
 echo -n $1 \
 | awk 'BEGIN{count=3231928;OFS=""}{while (++count>=0) print $0,count}' \
 | unbuffer -p awk '{ORS=""; cmd="md5sum"; print $0 | cmd; close(cmd)}' \
-| awk '{print NR " " substr($1,1,5)}'
+| awk '{ORS=""; if (substr($1,1,5) == "00000") print substr($1,6,1) "\n"}'
