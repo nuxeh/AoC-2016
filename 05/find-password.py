8b5da1eb5bced5f1d33b544a588ceb980ef8e601
Edward Cragg <drq.11235@gmail.com>
2016-12-05 20:53:12 +0000

    05-02: make password finding script continue when piped
    If the script is redirected, continue outputting valid ids

--- 05/find-password.py
+++ 05/find-password.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python2
 
 import md5
+import sys
 
 input = 'wtnhxymk'
 num = 0
 count = 0
 password = ""
+
 
 while True:
     m = md5.new()
@@ -15,7 +17,8 @@ while True:
     if hex_string[0:5] == '00000':
         password += hex_string[5]
 	print hex_string + " " + password
-	if len(password) == 8:
+	sys.stdout.flush()
+	if len(password) == 8 and sys.stdout.isatty():
             print "Password is: " + password
             exit()
 
