9319527225f96544a6643418462d9d8b165c5ddd
Edward Cragg <edward.cragg@codethink.co.uk>
2016-12-05 18:13:14 +0000

    05-01: set ORS in BEGIN{}
        $ ./find-password.sh abc
        1
        8
        f
        4

--- 05/find-password.sh
+++ 05/find-password.sh
@@ -5,4 +5,4 @@
 echo -n $1 \
 | awk 'BEGIN{count=3231928;OFS=""}{while (++count>=0) print $0,count}' \
 | stdbuf -oL -eL awk '{ORS=""; cmd="md5sum"; print $0 | cmd; close(cmd)}' \
-| awk '{ORS=""; if (substr($1,1,5) == "00000") print substr($1,6,1) "\n"}'
+| awk '{ORS=""}{if (substr($1,1,5) == "00000") print substr($1,6,1) "\n"}'
