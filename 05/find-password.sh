1a8fe1abca81b55fe42bb44deb3e9ab45730cb7b
Edward Cragg <edward.cragg@codethink.co.uk>
2016-12-05 15:49:32 +0000

    05-01: continue, get first 5 chars of sum
        $ ./find-password.sh abc | head
        1 00000
        2 7fb3f
        3 e4b19
        4 3b740
        5 be72d
        6 1323a
        7 57f04
        8 a23ba
        9 d04ee
        10 8927c

--- 05/find-password.sh
+++ 05/find-password.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Depends on ubuffer from expect package
+
 echo -n $1 \
-| awk 'BEGIN{count=-1;OFS=""}{while (++count>=0) print $0,count}' \
-| awk '{cmd="md5sum"; print $0 | cmd; close(cmd)}'
+| awk 'BEGIN{count=3231928;OFS=""}{while (++count>=0) print $0,count}' \
+| unbuffer -p awk '{ORS=""; cmd="md5sum"; print $0 | cmd; close(cmd)}' \
+| awk '{print NR " " substr($1,1,5)}'
