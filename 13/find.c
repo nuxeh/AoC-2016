44875b719384c8857a3ab682a7aaa9515ae3030a
Edward Cragg <edward.cragg@codethink.co.uk>
2016-12-13 09:41:52 +0000

    13-01: add simple cubicle generator and Makefile

---
+++ 13/find.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x=0; x<9; x++) {
+		for (y=0; y<6; y++) {
+			printf("x: %d y: %d\n", x, y);
+
+
+		}
+	}
+
+
+
+}
