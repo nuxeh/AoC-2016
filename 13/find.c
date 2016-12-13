fedb68b87c2ee0d739ad51bcd65bb14d6cb6873c
Edward Cragg <drq.11235@gmail.com>
2016-12-13 21:22:19 +0000

    13-01: wip

--- 13/find.c
+++ 13/find.c
@@ -9,19 +9,33 @@
 #define FAVENUM 10
 #endif
 
-char *num2bin(int num) {
-	static char out[32];
+#if 1
+#define DEBUG(s,f) printf(s,f)
+#else
+#define DEBUG(s,f)
+#endif
+
+char num2bin(int num, char *out) {
+	char *bitvalue;
+	char count;
+
 	out[0] = '\0';
+	count = 0;
 
 	unsigned int bit;
 	for (bit = pow(2, 31); bit > 0; bit >>= 1) {
-		printf("bit: %u\n", bit);
-		strcat(out, ((num & bit) == bit) ? "1" : "0");
+		bitvalue = ((num & bit) == bit) ? "1" : "0";
+		DEBUG("bit: %u\n", bit);
+
+		strcat(out, bitvalue);
+
+		if (bitvalue == "1")
+			count++;
 	}
 
 	/* printf("sizeof(int) %d\n", sizeof(int)); */
 
-	return out;
+	return count;
 }
 
 
@@ -30,13 +44,18 @@ int main(void)
 	int x;
 	int y;
 	int res;
+	char bin[32];
+	char count;
 
 	for (x=0; x<80; x++) {
 		for (y=0; y<40; y++) {
 			res = (x * x) + (3 * x) + (2 * x * y) + y + (y * y);
 			res += FAVENUM;
 
-			printf("x: %d y: %d res: %d bin: %s\n", x, y, res, num2bin(res));
+			count = num2bin(res, bin);
+			DEBUG("count: %d even: %d\n", count, count % 2);
+
+			printf("x: %d y: %d res: %d bin: %s\n", x, y, res, bin);
 		}
 	}
 
