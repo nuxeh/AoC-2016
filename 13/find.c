0afbfa6ce1c02221b68ea8f5378cbe98adc777c4
Edward Cragg <edward.cragg@codethink.co.uk>
2016-12-13 12:55:36 +0000

    13-01: some calculation

--- 13/find.c
+++ 13/find.c
@@ -1,16 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#if 0
+#define FAVENUM 1350
+#else
+#define FAVENUM 10
+#endif
+
+
 int main(void)
 {
 	int x;
 	int y;
+	int res;
 
 	for (x=0; x<9; x++) {
 		for (y=0; y<6; y++) {
-			printf("x: %d y: %d\n", x, y);
+			res = (x * x) + (3 * x) + (2 * x * y) + y + (y * y);
+			res += FAVENUM;
 
-
+			printf("x: %d y: %d res: %d\n", x, y, res);
 		}
 	}
 
