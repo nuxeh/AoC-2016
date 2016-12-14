3d873b1b26eea51750156dbdedf439ccb1c56b80
Edward Cragg <edward.cragg@codethink.co.uk>
2016-12-14 13:37:07 +0000

    14-01: wip zzzz

--- 14/find-key.py
+++ 14/find-key.py
@@ -13,7 +13,10 @@ key_first = {}
 rep_re_3 = re.compile(r'(.)\1{2}')
 rep_re_5 = re.compile(r'(.)\1{4}')
 
+keys_found = 0
+
 while True:
+    seed = input + str(num)
     m = md5.new()
     m.update(input + str(num))
     hex_string = m.hexdigest()
@@ -22,19 +25,34 @@ while True:
     key_parts_5 = [match.group() for match in rep_re_5.finditer(hex_string)]
 
     for index, part in enumerate(key_parts_3):
-	print input + str(num)
+	print seed
 	print hex_string
 	print str(index) + ": " + part
 
 	key_first[num] = (hex_string, part);
 
     for index, part in enumerate(key_parts_5):
-	print input + str(num)
+	print seed
 	print hex_string
 	print str(index) + ": " + part
 
-	for it_num, details in enumerate(key_first):
-		pass
+	for key in key_first:
+            print part
+            print part[0:3]
+            print key_first[key][1]
+            print
+            print key
+            print key_first[key]
+            print
+            print num
+
+	    if key_first[key][1] == part[0:3]:
+                keys_found += 1 
+                print("found key " + key_first[key][0] + " at " + str(input +
+                      str(key)) + " num: " + str(num))
+                print str(keys_found) + " keys found"
+
+            raw_input()
 
     num += 1
 
