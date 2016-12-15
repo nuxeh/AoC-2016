bc853671fcf18802c8a846972c679cad22bee1a0
Edward Cragg <drq.11235@gmail.com>
2016-12-15 01:37:50 +0000

    14-01: reduce insanity further

--- 14/find-key.py
+++ 14/find-key.py
@@ -5,10 +5,6 @@ import md5
 import sys
 import re
 
-def dump_keys(arr):
-    for key in sorted(arr):
-        print str(key) + ":\t" + arr[key][0] + " " + key_first[key][1]
-
 input = 'ahsbgdzn'
 input = 'abc'
 num = 0
@@ -31,11 +27,11 @@ while True:
     key_parts_3 = [match.group() for match in rep_re_3.finditer(hex_string)]
     key_parts_5 = [match.group() for match in rep_re_5.finditer(hex_string)]
 
-    for index, part in enumerate(key_parts_5):
-	found_keys_5.append((hex_string, part, num));
+    if len(key_parts_5) > 0:
+        found_keys_5.append((hex_string, key_parts_5[0], num));
 
-    for index, part in enumerate(key_parts_3):
-	found_keys_3.append((hex_string, part, num));
+    if len(key_parts_3) > 0:
+        found_keys_3.append((hex_string, key_parts_3[0], num));
 
 
     num += 1
@@ -44,42 +40,16 @@ while True:
 
 
 for three in found_keys_3:
-    print three
 
-for five in found_keys_5:
-    print five
+    for five in found_keys_5:
+        if three[1][0] == five[1][0]:
+            if five[2] - three[2] <= 1000:
+                print three
+		keys_found += 1
 
-if False:
+		if keys_found == 64:
+                    print three[2]
+                    exit(0)
 
-
-    for key in sorted(key_first):
-        hash_str  = key_first[key][0]
-        match     = key_first[key][1]
-    
-        if match == part[0:3] and (num - key) <= 1000:
-            print "original  " + hex_string + " diff: " + str(num - key)
-    
-            keys_found += 1
-    
-            print("found key " + hash_str + " at " + str(input +
-                  str(key)) + " num: " + str(num))
-     
-            print str(keys_found) + " keys found " + str(len(found_keys)) 
-    
-            if key in found_keys.keys():
-                print "dupe!!"
-                #found_keys[str(key) + "_"] = hash_str
-    		#else:
-            found_keys[key] = hash_str
-    
-            #dump_keys()
-            dump_keys_found()
-    
-            if len(found_keys) > 64+1:
-                print "Found 64 keys at index: " + str(key)
-                exit(0)
-            #break
-    
-        raw_input()
 
 # http://stackoverflow.com/questions/6306098/regexp-match-repeated-characters
